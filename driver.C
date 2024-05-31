4763b66e8cfeaccca4da516e90ebc37f7cadf975
Raj Handique <raj.handique519@deccansociety.org>
2024-05-31 20:35:15 +0530

    Added Analysis Code

--- driver.C
+++ driver.C
@@ -5,27 +5,17 @@ void driver(int sample=0)
 {
   const char *hstfilename, *sumfilename; 
   TChain *chain = new TChain("Events/Events");
-  MinBiasAna m_selec;//declared an instance of our class.
+  cmsAna m_selec;//declared an instance of our class.
   
   //--------------------------------------------------------------------------//
   //               Choose the sample you want to run over                     //
   //--------------------------------------------------------------------------//
 
 
-  if(sample==0){//7 TeV
-    chain->Add("InputFiles/CMS_Run2011A_MinimumBias_AOD_12Oct2013-v1_250Kevts.root");
-    chain->Add("InputFiles/CMS_Run2011B_MinimumBias_AOD_12Oct2013-v1_250Kevts.root");
-    hstfilename = "OutputFiles/hst_QCD_7.root"; 
+  if(sample==0){
+    chain->Add("testoutput.root");
+    hstfilename = "test_hist.root"; 
   }  
-
-  if(sample==1){//8 TeV
-    chain->Add("InputFiles/CMS_Run2012A_MinimumBias_AOD_22Jan2013-v1_250Kevts.root");
-    chain->Add("InputFiles/CMS_Run2012B_MinimumBias_AOD_22Jan2013-v1_250Kevts.root");
-    chain->Add("InputFiles/CMS_Run2012C_MinimumBias_AOD_22Jan2013-v1_250Kevts.root");
-    chain->Add("InputFiles/CMS_Run2012D_MinimumBias_AOD_22Jan2013-v1_250Kevts.root");
-    hstfilename = "OutputFiles/hst_QCD_8.root"; 
-
-  }
 
   std::cout<<"Output : "<<hstfilename<<std::endl;
   m_selec.SetHstFileName(hstfilename);
